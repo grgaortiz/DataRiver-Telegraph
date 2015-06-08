b3607c1946a22ca32f982a5fef78f62ab4a70efe
grgaortiz <grgaortiz@gmail.com>
2015-06-08 15:55:50 -0600

    works

--- index.js
+++ index.js
@@ -19,39 +19,45 @@ var sendgrid = require('sendgrid')('grgortiz', '3ataRiver!');
  * Params
  */
 var asyncFunctions = [];
+var emails = [];
 var n = 0;
+var i = 0;
+
 
 /**
  * Telegraph
  */
-var file = fs.readFileSync('emailTest.csv');
-csv.parse(file, {delimiter: ','}, function (err, data) {
+var file = fs.readFileSync('/Users/gortiz/mean/DataRiver-Telegraph/emailTest.csv');
+csv.parse(file, function (err, data) {
     if(err) {
         return console.log(err);
     } else {
         for (var key in data) {
+            var row = data[key];
             if (data.hasOwnProperty(key)) {
-                var h = data[key];
-                if (h[0] != '') {
-                    asyncFunctions.push(function (callback) {
-                        // SendGrid
-                        sendgrid.send({
-                            to: '' + h[0] + '',
-                            from: 'grgaortiz@test.com',
-                            subject: 'NodeTest',
-                            text: 'My first email through SendGrid.'
-                        }, function (err, json) {
-                            if (err) {
-                                console.error(err);
-                                callback();
-                            } else {
-                                n++;
-                                callback();
-                            }
-                        });
-                    });
+                if (row[0] != '') {
+                    emails.push(row[0]);
                 }
             }
+            asyncFunctions.push(function (callback) {
+                var email = emails[i];
+                // SendGrid
+                sendgrid.send({
+                    to: '' + email + '',
+                    from: 'grgaortiz@test.com',
+                    subject: 'NodeTest',
+                    text: 'My first email through SendGrid.'
+                }, function (err, json) {
+                    if (err) {
+                        console.error(err);
+                        callback();
+                    } else {
+                        n++;
+                        callback();
+                    }
+                });
+                i++;
+            });
         }
 
         async.parallel(
