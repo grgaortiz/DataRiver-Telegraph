c6b0c84471d12edc674d1e80407e90cb3dfadce0
grgaortiz <grgaortiz@gmail.com>
2015-06-09 14:20:52 -0600

    ready

--- index.js
+++ index.js
@@ -19,41 +19,45 @@ var sendgrid = require('sendgrid')(config.SendGridUser, config.SendGridPass);
  * Params
  */
 var n = 0;
+var r = 0;
 
 /**
  * Telegraph
  */
-fs.readFile('testEmails.csv', 'utf8', function(error, data) {
+fs.readFile('hospitalEmails.csv', 'utf8', function(error, data) {
     csv.parse(data, function (err, data) {
         if (err) {
             return console.log(err);
         } else {
             for (var key in data) {
-                var row = data[key];
-                if (data.hasOwnProperty(key)) {
-                    if (row[0] != '') {
-                        // SendGrid
-                        var email = new sendgrid.Email({
-                            to: '' + row[0] + '',
-                            from: '' + config.emailFrom + '',
-                            fromname: '' + config.emailFromName + '',
-                            subject: '' + config.emailSubject + '',
-                            text: '' + config.emailText + ''
-                        });
-                        email.addCategory('SLA Webinar');
-                        email.addCategory('Telegraph');
-                        sendgrid.send(email, function (err, json) {
-                            if (err) {
-                                console.error(err);
-                            } else {
-                                n++;
-                                console.error(row[0], json, n);
-                            }
-                        });
+                // skip header
+                if(r > 0) {
+                    var row = data[key];
+                    if (data.hasOwnProperty(key)) {
+                        if (row[0] != '') {
+                            // SendGrid
+                            var email = new sendgrid.Email({
+                                to: '' + row[0] + '',
+                                from: '' + config.emailFrom + '',
+                                fromname: '' + config.emailFromName + '',
+                                subject: '' + config.emailSubject + '',
+                                text: '' + config.emailText + ''
+                            });
+                            email.addCategory('SLA Webinar');
+                            email.addCategory('Telegraph');
+                            sendgrid.send(email, function (err, json) {
+                                if (err) {
+                                    console.error(err);
+                                } else {
+                                    n++;
+                                    console.error(json, n);
+                                }
+                            });
+                        }
                     }
                 }
+                r++;
             }
-
         }
     });
 });
